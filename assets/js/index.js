f61fa8a8b8a382a4cc7fd7fbac3e90fe216bd542
hoanganhquang <anhquang976@gmail.com>
2022-06-04 15:49:58 +0700

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,34 @@
 const navbar = document.getElementById("navbar");
 const navitem = document.querySelectorAll(".header-features .nav .item");
+const menuBtn = document.querySelector(".menu-btn");
+const menuRes = document.getElementById("menu-responsive");
 
-const checkScroll = () => {
-  document.addEventListener("scroll", () => {
-    if (window.scrollY > 200) {
-      navbar.style.boxShadow = "0 0 .625rem var(--black-color)";
-    } else {
-      navbar.style.boxShadow = "unset";
-    }
+document.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    navbar.style.boxShadow = "0 0 .625rem var(--black-color)";
+  } else {
+    navbar.style.boxShadow = "unset";
+  }
+});
+
+navitem.forEach((item) => {
+  item.addEventListener("click", () => {
+    const curActive = document.getElementsByClassName("active");
+    curActive[0].classList.remove("active");
+    item.classList.add("active");
   });
-};
+});
 
-const checkActive = () => {
-  navitem.forEach((item) => {
-    item.addEventListener("click", () => {
-      const curActive = document.getElementsByClassName("active");
-      curActive[0].classList.remove("active");
-      item.classList.add("active");
-    });
-  });
-};
+menuBtn.addEventListener("click", () => {
+  menuRes.classList.toggle("show");
+});
 
-checkScroll();
-checkActive();
+document.addEventListener("click", (e) => {
+  console.log();
+  if (
+    menuRes.classList.contains("show") &&
+    e.target.classList.value != "fa-solid fa-ellipsis"
+  ) {
+    menuRes.classList.remove("show");
+  }
+});
